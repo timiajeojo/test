a5788d6a1b6d18e991ed5421cab8adb27578b8f2
Timi Ajeojo <timiajeojo@gmail.com>
2024-02-08 10:31:56 +0100

    Update index.js

--- test/index.js
+++ test/index.js
@@ -1,10 +1,7 @@
-let sentence = ["Hello ", "my ", "name ", "is ", "Per"] 
+let sentence = ["Hello", "my", "name", "is", "Per"] 
 let greetingEl = document.getElementById("greeting-el")
-let sentenceText = ""
 
 for (let i = 0; i < sentence.length; i++) {
-sentenceText += sentence[i];
-   
-}
+    greetingEl.textContent += sentence[i] + " "
 
-greetingEl.textContent = sentenceText;
+}
